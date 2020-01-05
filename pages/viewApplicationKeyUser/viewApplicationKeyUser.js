66d24b879e370a0d91f78a07e2dabe1eb3be5cb7
wuxw <928255095@qq.com>
2020-01-05 14:11:54 +0800

    开门功能完善

--- pages/viewApplicationKeyUser/viewApplicationKeyUser.js
+++ pages/viewApplicationKeyUser/viewApplicationKeyUser.js
@@ -39,9 +39,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      idCard: options.idCard
-    });
+    // this.setData({
+    //   idCard: options.idCard
+    // });
 
   },
 
