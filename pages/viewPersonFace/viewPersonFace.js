91740d20096c3e8ccb88d4f32f390376895be725
wuxw <928255095@qq.com>
2020-01-06 14:20:41 +0800

    人脸页面开发完成

--- pages/viewPersonFace/viewPersonFace.js
+++ pages/viewPersonFace/viewPersonFace.js
@@ -5,14 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      face:null,
+      btnValue:"采集人脸"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      this.setData({
+        face:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578300447436&di=e20a0afaadbfd1c48b9de834c35da9ee&imgtype=0&src=http%3A%2F%2F00.minipic.eastday.com%2F20170524%2F20170524155526_e561e179dc10040e208de068f528e3b3_3.jpeg'
+      });
   },
 
   /**
@@ -26,7 +29,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+      this.loadOwnerFace();
   },
 
   /**
@@ -61,6 +64,16 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+  collectFace:function(){
+    //采集人脸
+    console.log('采集人脸');
+  },
+  /**
+   * 查询业主人脸
+   */
+  loadOwnerFace:function(){
 
   }
 })
