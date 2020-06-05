8c0869021379ebcb17ab52894cd6b5e41ad1a633
java110 <928255095@qq.com>
2020-06-05 17:06:16 +0800

    加入警告

--- constant/AppConstant.js
+++ constant/AppConstant.js
@@ -8,6 +8,10 @@ class AppConstant{
 
   /**
  * 应用信息，主要是用来和后端服务交互时的时候用
+ * 
+ * 这里请不要修改，这个不是微信的AppId 和appSecurity 这个是HC小区管理系统分配的
+ * 这里请不要修改，这个不是微信的AppId 和appSecurity 这个是HC小区管理系统分配的
+ * 这里请不要修改，这个不是微信的AppId 和appSecurity 这个是HC小区管理系统分配的
  */
   static appId = "992019111758490006";
   static appSecurity = "";
