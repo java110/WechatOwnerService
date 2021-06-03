9a4e9525a997a746923f8a06f605e7f11615635a
java110 <928255095@qq.com>
2021-06-04 02:04:47 +0800

    引入打通

---
+++ pages/hcWebView/hcWebView.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="h5-html">
+		 <web-view ref="webview"  :src="url"  @message="reciveMessage"></web-view>
+	</view>
+</template>
+
+<script>
+	import conf from '../../conf/config.js'
+	export default {
+		data() {
+			return {
+				url: ''
+			}
+		},
+		onLoad(options) {
+			this.url = options.url;
+		
+		},
+		methods: {
+			reciveMessage: function(event) {
+				console.log('商城回传的参数', event);
+			}
+		}
+	}
+</script>
+
+<style>
+	.h5-html {
+		width: 100%;
+		height: 100%;
+	}
+</style>
