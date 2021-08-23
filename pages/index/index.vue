243125d8f4740c4bbe56552c32869a5627ea00f6
java110 <928255095@qq.com>
2021-08-23 12:06:41 +0800

    优化代码

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<view class="margin-bottom-xs">
-			<uni-notice-bar showIcon="true" scrollable="true" single="true" speed="30" text="欢迎访问德坤云物管"></uni-notice-bar>
+			<uni-notice-bar showIcon="true" scrollable="true" single="true" speed="30" text="欢迎访问HC智慧家园"></uni-notice-bar>
 		</view>
 
 		<scroll-view @scrolltolower="lower" class="scroll-restaurants-list" scroll-y="true" style="height:100%">
