7cbcb9d6927c4595473cae550a139acb9937dfd5
java110 <928255095@qq.com>
2021-09-03 02:42:01 +0800

    优化支付

--- factory/WexinPayFactory.js
+++ factory/WexinPayFactory.js
@@ -26,9 +26,17 @@ const wx = require('jweixin-module');
 class WexinPayFactory {
 	wexinPay(data, successCallBack) {
 
-		let [appId, timestamp, nonceStr, signature, packages, paySign] = [data.appId, data.timeStamp, data.nonceStr, data.sign,
-			data.package, data.sign
+		let [appId, timestamp, nonceStr, signature, packages, paySign,signType] = [data.appId, data.timeStamp, data.nonceStr, data.sign,
+			data.package, data.paySign,data.signType
 		];
+		
+		if(!signType){
+			signType = 'MD5';
+		}
+		
+		if(!paySign){
+			paySign = signature;
+		}
 
 		wx.config({
 			debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
@@ -44,7 +52,7 @@ class WexinPayFactory {
 				timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
 				nonceStr, // 支付签名随机串，不长于 32 位
 				'package': packages, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-				signType: 'MD5', // 签名方式，默认为‘SHA1‘，使用新版支付需传入‘MD5‘
+				signType: signType, // 签名方式，默认为‘SHA1‘，使用新版支付需传入‘MD5‘
 				paySign, // 支付签名
 				success(res) {
 					// 支付成功后的回调函数
