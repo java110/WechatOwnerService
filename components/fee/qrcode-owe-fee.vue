1f84fa3ae31b511f3a838268785507b387b11da8
Your Name <you@example.com>
2023-09-19 00:48:17 +0800

    优化业主功能

--- components/fee/qrcode-owe-fee.vue
+++ components/fee/qrcode-owe-fee.vue
@@ -45,7 +45,7 @@
 								<text class="margin-right-xs">业主:{{item.ownerName}}</text>
 							</view>
 							<view class="sub-info-item text-gray text-sm">
-								<text class="margin-right-xs">单价:{{item.squarePrice}}</text>
+								<text class="margin-right-xs">单价:{{item.mwPrice && item.mwPrice>0?item.mwPrice:item.squarePrice}}/{{item.additionalAmount}}</text>
 							</view>
 							<view class="sub-info-item text-gray text-sm" v-if="item.payerObjName">
 								<text class="margin-right-xs">房号:{{item.payerObjName}}</text>
